825069058cc532cae4927b7d7dc8974840b60201
rizkymalm <rizkymalm@gmail.com>
2021-09-17 03:14:40 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -4,8 +4,7 @@ const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database:'idd3735_jimny',
-    port: 8889
+    database:'idd3735_jimny'
 })
 
 con.connect(err => {
